6d5a187d7bc62030cdd4004551b5f34f58a5e590
Mehmet Emin Aydogdu <230254054@ogr.alanya.edu.tr>
2026-04-07 21:31:54 +0300

    fix: firebase install and config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,6 +3,16 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import firebaseConfig from '../../firebase-applet-config.json';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCrT6eaWUzx5pIobHhIv62D8hu5E1by3Xw",
+  authDomain: "kisisel-blog-73478.firebaseapp.com",
+  projectId: "kisisel-blog-73478",
+  storageBucket: "kisisel-blog-73478.firebasestorage.app",
+  messagingSenderId: "716734159365",
+  appId: "1:716734159365:web:dcf1dfbc1ff48605271a6c",
+  measurementId: "G-F3SCH231V4"
+};
+
 const app = initializeApp(firebaseConfig);
 
 // Initialize Firestore with the specific database ID from config
